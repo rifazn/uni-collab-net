a15ba2b3c96f2f79ebee14ef2af38a0fa1e531cb
Sourav <souravsahatonmoy@gmail.com>
2017-12-13 19:43:10 +0600

    something updated

--- index.php
+++ index.php
@@ -5,29 +5,22 @@
         <meta charset="utf-8">
     </head>
     <body>
-        <nav>
-        <ul>
-        <!-- <?php while ($row = $stmt->fetchcolumn() ) {
-		    $output = "<li><a href=local_wb.php?c=";
-		    $output .= "$row>$row</a></li>";
-		   
-		   
-		    //$output = $row;
-		    echo $output;
-        }
-        ?> -->
-        	<li>Blog</li>
-        	<li>Courses you are doing</li>
-        	<li>Course Resources</li>
-        	<li>Your Cloud</li>
-        	<li>Classmates from your courses</li>
-        </ul>
-        </nav>
         
         <main>
-        	<h1>Global Whiteboard</h1>
+        	<h1>Uni Collab Net</h1>
         	<small>Find out what's going on around your Uni.</small>
         </main>
+        
+        <nav>
+		    <ul>
+		    
+		    	<li>Blog</li>
+		    	<li>Courses you are doing</li>
+		    	<li>Course Resources</li>
+		    	<li>Your Cloud</li>
+		    	<li>Classmates from your courses</li>
+		    </ul>
+        </nav>
         
         <aside>
         	<?php echo "Hello, Sourav Saha"; ?>
