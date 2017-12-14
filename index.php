039abc6d0767e4f54704d46b0fcded40f9f6f177
Rifaz Nahiyan <rifazn@protonmail.com>
2017-12-14 12:48:51 +0600

    just added a div to the index.php to make it easier to apply css grid.

--- index.php
+++ index.php
@@ -5,27 +5,30 @@
         <meta charset="utf-8">
     </head>
     <body>
-        
-        <main>
-        	<h1>Uni Collab Net</h1>
-        	<small>Find out what's going on around your Uni.</small>
-        </main>
-        
-        <nav>
-		    <ul>
-		    
-		    	<li>Blog</li>
-		    	<li>Courses you are doing</li>
-		    	<li>Course Resources</li>
-		    	<li>Your Cloud</li>
-		    	<li>Classmates from your courses</li>
-		    </ul>
-        </nav>
-        
-        <aside>
-        	<?php echo "Hello, Sourav Saha"; ?>
-        	<button>Logout</button>
-        </aside>
 
+		<div id="content">
+			
+			<main>
+				<h1>Uni Collab Net</h1>
+				<small>Find out what's going on around your Uni.</small>
+			</main>
+			
+			<nav>
+				<ul>
+				
+					<li>Blog</li>
+					<li>Courses you are doing</li>
+					<li>Course Resources</li>
+					<li>Your Cloud</li>
+					<li>Classmates from your courses</li>
+				</ul>
+			</nav>
+			
+			<aside>
+				<?php echo "Hello, Sourav Saha"; ?>
+				<button>Logout</button>
+			</aside>
+
+		</div>
     </body>
 </html>
